b28724846f0f0980021367ff61aad7729033eaa3
Heiquedev <heiquerege@gmail.com>
2025-07-31 12:00:09 -0300

    adicionando duas novas rotas na web.php

--- emma/routes/web.php
+++ emma/routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\GoogleAuthController;
 
 Route::middleware(['web'])->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
+    Route::get('/auth/google/redirect', [GoogleAuthController::class, 'redirectToGoogle']);
+    Route::get('/auth/google/callback', [GoogleAuthController::class, 'handleGoogleCallback']);
 });
